4a2bf595de7f429f04f2c198484e8e04704be7bf
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-15 16:42:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+  attr_accessor :title
+  def initialize title 
+    @title = title
+  end 
+  def title 
+    @title
+  end 
+  def author_name
+  end 
+  def page_count
+    
+end 
+
+
+
 
 
